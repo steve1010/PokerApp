11984264748e2d8ef3781df3214db89e82f44c7b
Steve1010 <steve1010>
2017-10-31 21:49:25 +0100

    refactorings in GameServer: extracted private methods
    -> next coming up is creating IMViews in client.gameplay.Gameplay.fxml
    to display the btn or blind(s) and to add functionality to 'em.
    - remind that sb is dealer in heads-up!
    
    - issue added

--- Server/src/backend/GameServer.java
+++ Server/src/backend/GameServer.java
@@ -48,47 +48,133 @@ public class GameServer extends ClientHandler implements RemoteAccess {
 
 	@Override
 	public void run() {
-		// game starting..
-		game.getPlayersList()
-				.forEach(p -> System.out.println("Player: " + p.getName() + " joined game: " + game.getName() + ".\n"));
+
+		printPlayers();
+
 		while (running) {
+			List<SafePlayer> orderedPlayers = distributeCards();
 
-			List<SafePlayer> orderedPlayers = chooseBtnPlayerRandomly();
-			game = new Game(game.getName(), game.getId(), game.getBuyIn(), game.getStartChips(), game.getMaxPlayers(),
-					game.getPaid(), game.getSignedUp(), orderedPlayers);
-
-			orderedPlayers.forEach(player -> {
-				PlayerHand playerHand = new PlayerHand(player.getId(), dealer.newCard(), dealer.newCard());
-				playerHandsList.add(playerHand);
-			});
-			triggerPlayerHands();
-			setBoard(new Board(dealer.newCard(), dealer.newCard(), dealer.newCard(), dealer.newCard(),
-					dealer.newCard()));
 			for (int roundCounter = 0; roundCounter < 5 && playerHandsList.size() > 1; roundCounter++) {
 				MinRoundBet minRoundBet = new MinRoundBet(0);
-				for (int playerID = 0; playerID < orderedPlayers.size(); playerID++) {
+				for (int player = 0; player < orderedPlayers.size(); player++) {
 
-					InetSocketAddress asyncGameClientAddress = new InetSocketAddress(
-							orderedPlayers.get(playerID).getAdress().getAddress(),
-							(orderedPlayers.get(playerID).getAdress().getPort() + 2));
+					InetSocketAddress asyncGameClientAddress = getClientAdress(orderedPlayers.get(player));
 					if (roundCounter == 0) {
-						// pre-pre-flop
-						answer(new GamesServerMsg(null, playerID, GameMsgType.YOUR_POSITION), asyncGameClientAddress);
+						/** pre-pre-flop */
+						answer(new GamesServerMsg(null, player, GameMsgType.YOUR_POSITION), asyncGameClientAddress);
 					} else {
-						// r=1:pre-flop
-						// r=2:flop
-						// r=3:turn
-						// r=4:river
+						/**
+						 * r=1:pre-flop<br>
+						 * r=2:flop<br>
+						 * r=3:turn<br>
+						 * r=4:river
+						 */
 						answer(new GamesServerMsg(null, -5, GameMsgType.YOUR_TURN, minRoundBet),
 								asyncGameClientAddress);
 						new Thread(new PlayersActionClientHandler(receiveObject(), game.getPlayersList(),
 								playerHandsList, minRoundBet)).start();
 					}
 				}
-				roundCounter++;
 			}
 			running = false;
 		}
+	}
+
+	private InetSocketAddress getClientAdress(SafePlayer safePlayer) {
+		InetSocketAddress asyncGameClientAddress = new InetSocketAddress(safePlayer.getAdress().getAddress(),
+				(safePlayer.getAdress().getPort() + 2));
+		return asyncGameClientAddress;
+	}
+
+	private List<SafePlayer> distributeCards() {
+		List<SafePlayer> orderedPlayers = distributePlayerCardsServerside();
+		triggerPlayerHands();
+		setBoard(new Board(dealer.newCard(), dealer.newCard(), dealer.newCard(), dealer.newCard(), dealer.newCard()));
+		return orderedPlayers;
+	}
+
+	@Override
+	public void shutdown() {
+		this.running = false;
+	}
+
+	/**
+	 * timer functionality: Games start on last player registered plus 30seconds or
+	 * on all players clicked 'im ready'
+	 */
+	private void triggerPlayerHands() {
+		runPlayersReady();
+
+		int playerID = 0;
+		for (SafePlayer player : game.getPlayersList()) {
+			InetSocketAddress asyncGameplayPlayerAddress = new InetSocketAddress(player.getAdress().getAddress(),
+					(player.getAdress().getPort() + 2));
+			answer(new GamesServerMsg(MsgType.GAMES_SERVER_MSG, -5, GameMsgType.YOUR_HAND,
+					playerHandsList.get(playerID)), asyncGameplayPlayerAddress);
+
+			System.err.println("GameServer answered to client-port: " + asyncGameplayPlayerAddress.toString());
+
+			playerID++;
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void runPlayersReady() {
+		notAllPlayersReady = true;
+		runCountingThread();
+		while (notAllPlayersReady) {
+			PlayersActionQuery playerQuery = receiveObject();
+			if (Objects.nonNull(playerQuery)) {
+				if (playerQuery.getOption().equals(Option.READY)) {
+					readyCounter.incrementAndGet();
+					if (readyCounter.get() == game.getPlayersList().size()) {
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	private void runCountingThread() {
+		new Thread(() -> {
+			try {/** 30 seconds */
+				Thread.sleep(30000);
+				answer(new ServerMsg(null, port), new InetSocketAddress("localhost", port));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}).start();
+	}
+
+	private List<SafePlayer> distributePlayerCardsServerside() {
+		List<SafePlayer> orderedPlayers = chooseBtnPlayerRandomly();
+		game = new Game(game.getName(), game.getId(), game.getBuyIn(), game.getStartChips(), game.getMaxPlayers(),
+				game.getPaid(), game.getSignedUp(), orderedPlayers);
+
+		orderedPlayers.forEach(player -> {
+			PlayerHand playerHand = new PlayerHand(player.getId(), dealer.newCard(), dealer.newCard());
+			playerHandsList.add(playerHand);
+		});
+		return orderedPlayers;
+	}
+
+	/**
+	 * Returns the re-ordered playersList with the Btn player at index 0.
+	 */
+	private List<SafePlayer> chooseBtnPlayerRandomly() {
+		List<SafePlayer> players = new ArrayList<>();
+		int size = game.getPlayersList().size() - 1;
+		for (int i = size; i >= 0; i--) {
+			int random = (int) (Math.random() * i);
+			players.add(game.getPlayersList().get(random));
+			System.out.println("player added..");
+			game.getPlayersList().remove(random);
+		}
+		return players;
 	}
 
 	private PlayersActionQuery receiveObject() {
@@ -114,68 +200,14 @@ public class GameServer extends ClientHandler implements RemoteAccess {
 		return null;
 	}
 
+	private void printPlayers() {
+		game.getPlayersList()
+				.forEach(p -> System.out.println("Player: " + p.getName() + " joined game: " + game.getName() + ".\n"));
+	}
+
 	/**
-	 * Returns the re-ordered playersList with the Btn player at index 0.
+	 * Getters & Setters
 	 */
-	private List<SafePlayer> chooseBtnPlayerRandomly() {
-		List<SafePlayer> players = new ArrayList<>();
-		int size = game.getPlayersList().size() - 1;
-		for (int i = size; i >= 0; i--) {
-			int random = (int) (Math.random() * i);
-			players.add(game.getPlayersList().get(random));
-			System.out.println("player added..");
-			game.getPlayersList().remove(random);
-		}
-		return players;
-	}
-
-	private void triggerPlayerHands() {
-		notAllPlayersReady = true;
-		/**
-		 * timer functionality: Games should start on last player registered plus
-		 * 30seconds or on all players clicking 'im ready'
-		 */
-		new Thread(() -> {
-			try {// 30 seconds
-				Thread.sleep(30000);
-				answer(new ServerMsg(null, port), new InetSocketAddress("localhost", port));
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}).start();
-		while (notAllPlayersReady) {
-			PlayersActionQuery playerQuery = receiveObject();
-			if (Objects.nonNull(playerQuery)) {
-				playerQuery.getOption();
-				if (playerQuery.getOption().equals(Option.READY)) {
-					readyCounter.incrementAndGet();
-					if (readyCounter.get() == game.getPlayersList().size()) {
-						break;
-					}
-				}
-			}
-		}
-
-		int playerID = 0;
-		for (SafePlayer player : game.getPlayersList()) {
-			InetSocketAddress asyncGameplayPlayerAddress = new InetSocketAddress(player.getAdress().getAddress(),
-					(player.getAdress().getPort() + 2));
-			answer(new GamesServerMsg(MsgType.GAMES_SERVER_MSG, -5, GameMsgType.YOUR_HAND,
-					playerHandsList.get(playerID)), asyncGameplayPlayerAddress);
-			System.err.println("GameServer answered to client-port: " + asyncGameplayPlayerAddress.toString());
-			playerID++;
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	@Override
-	public void shutdown() {
-		this.running = false;
-	}
 
 	public Game getGame() {
 		return game;
